264831e21d7c28c4ae8d6c1e5059fab51ce0d157
110B13203 <112997321+110B13203@users.noreply.github.com>
2024-04-09 10:37:30 +0800

    2.sql

--- Lab05/2.sql
+++ Lab05/2.sql
@@ -1,5 +1,5 @@
-CREATE VIEW ShohinSum (shohin_catalg, cnt_shohin)
+CREATE VIEW ShohinSumJim (shohin_catalg, cnt_shohin)
 AS
-SELECT shohin_catalg, COUNT(*)
-  FROM Shohin
- GROUP BY shohin_catalg;
+SELECT shohin_catalg, cnt_shohin
+  FROM ShohinSum
+ WHERE shohin_catalg = '辦公用品';
